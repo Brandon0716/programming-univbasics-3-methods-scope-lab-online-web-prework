abb78406da01be156811763b3407fd27f75b4760
essential-robot-4364 <brandon.james414@gmail.com>
2019-09-25 16:43:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,10 @@
-describe "any_phrase" do
-  it "takes in an argument and puts out the catch phrase" do
-    phrase = "Do A Barrel Roll!"
-    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
-  end
+def mario
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts phrase = "It's-a me, Mario!"
+  expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+end
+end
+
+def toadstool
+  puts status
 end
